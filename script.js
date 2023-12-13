08274d00b9747012f9602c2199cde73456ff0ed4
GlenCocopop <126820343+GlenCocopop@users.noreply.github.com>
2023-12-13 15:59:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Ta en bild när knappen klickas
+
+
 snapButton.addEventListener('click', function() {
     context.drawImage(video, 0, 0, 640, 480);
 
@@ -24,5 +26,16 @@ snapButton.addEventListener('click', function() {
     const colorThief = new ColorThief();
     const dominantColors = colorThief.getPalette(canvas, 6); // Ändra 6 till det antal färger du vill ha
 
-    console.log(dominantColors); // Visar en array av RGB-färgvärden
+    // Visa färgerna på webbsidan
+    const colorsDiv = document.getElementById('colors');
+    colorsDiv.innerHTML = ''; // Rensa befintligt innehåll
+    dominantColors.forEach(color => {
+        const colorDiv = document.createElement('div');
+        colorDiv.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+        colorDiv.style.width = '100px';
+        colorDiv.style.height = '100px';
+        colorDiv.style.display = 'inline-block';
+        colorDiv.style.margin = '10px';
+        colorsDiv.appendChild(colorDiv);
+    });
 });
